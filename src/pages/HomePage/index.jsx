f6560a7a33f82af64eefa14fba4e926fdc0b9416
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-04 05:47:11 -0300

    refactor: Redefinições gerais em HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,26 +1,52 @@
-import { useState } from "react";
-import { CartModal } from "../../components/CartModal";
-import { Header } from "../../components/Header";
-import { ProductList } from "../../components/ProductList";
+import styles from './style.module.scss';
+import { useEffect, useState } from 'react';
+import { productsApi } from '../../services/api';
+import { Header } from '../../components/Header';
+import { CartModal } from '../../components/CartModal';
+import { ProductList } from '../../components/ProductList';
 
 export const HomePage = () => {
-   const [productList, setProductList] = useState([]);
-   const [cartList, setCartList] = useState([]);
+	const [productList, setProductList] = useState([]);
+	const [cartList, setCartList] = useState([]);
+	const [isVisible, setIsVisible] = useState(false);
 
-   // useEffect montagem - carrega os produtos da API e joga em productList
-   // useEffect atualização - salva os produtos no localStorage (carregar no estado)
-   // adição, exclusão, e exclusão geral do carrinho
-   // renderizações condições e o estado para exibir ou não o carrinho
-   // filtro de busca
-   // estilizar tudo com sass de forma responsiva
+	const handleCartButtonClick = () => {
+		setIsVisible(true);
+	};
 
-   return (
-      <>
-         <Header />
-         <main>
-            <ProductList productList={productList} />
-            <CartModal cartList={cartList} />
-         </main>
-      </>
-   );
+	const handleCloseModal = () => {
+		setIsVisible(false);
+	};
+
+	useEffect(() => {
+		const getProducts = async () => {
+			try {
+				const { data } = await productsApi.get('/products');
+				setProductList(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getProducts();
+	}, []);
+
+	// useEffect montagem - carrega os produtos da API e joga em productList
+	// useEffect atualização - salva os produtos no localStorage (carregar no estado)
+	// adição, exclusão, e exclusão geral do carrinho
+	// renderizações condições e o estado para exibir ou não o carrinho
+	// filtro de busca
+	// estilizar tudo com sass de forma responsiva
+
+	return (
+		<>
+			<Header onCartButtonClick={handleCartButtonClick} />
+			<main className={styles.container}>
+				<ProductList productList={productList} />
+				{isVisible ? (
+					<CartModal cartList={cartList} onClose={handleCloseModal} />
+				) : null}
+			</main>
+		</>
+	);
 };
