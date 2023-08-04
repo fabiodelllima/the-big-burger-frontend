6c74e1c775b8637e396a5e909d805b23c838d5ec
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-04 05:46:06 -0300

    refactor: Redefinições gerais em Products

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -1,13 +1,22 @@
+import styles from './style.module.scss';
+
 export const ProductCard = ({ product }) => {
-    return(
-        <li>
-            <img src={product.img} alt={product.name} />
-            <div>
-                <h3>{product.name}</h3>
-                <span>{product.category}</span>
-                <span>{product.price.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
-                <button>Adicionar</button>
-            </div>
-        </li>
-    )
-}
+	return (
+		<li className={styles.container}>
+			<div className={styles.imgContainer}>
+				<img src={product.img} alt={product.name} />
+			</div>
+			<div className={styles.description}>
+				<h3 className={styles.name}>{product.name}</h3>
+				<span className={styles.category}>{product.category}</span>
+				<span className={styles.price}>
+					{product.price.toLocaleString('pt-BR', {
+						style: 'currency',
+						currency: 'BRL',
+					})}
+				</span>
+				<button className={styles.button}>Adicionar</button>
+			</div>
+		</li>
+	);
+};
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,11 +1,12 @@
-import { ProductCard } from "./ProductCard";
+import styles from './style.module.scss';
+import { ProductCard } from './ProductCard';
 
 export const ProductList = ({ productList }) => {
-   return (
-      <ul>
-         {productList.map((product) => (
-            <ProductCard key={product.id} product={product} />
-         ))}
-      </ul>
-   );
+	return (
+		<ul className={styles.container}>
+			{productList.map((product) => (
+				<ProductCard key={product.id} product={product} />
+			))}
+		</ul>
+	);
 };
