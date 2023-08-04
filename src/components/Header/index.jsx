86762855a3702605d802e57b9214a90f12396bd4
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-04 05:44:01 -0300

    refactor: Redefinições gerais em Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,40 @@
-import { useState } from "react";
-import Logo from "../../assets/Logo.svg";
-import { MdSearch, MdShoppingCart } from "react-icons/md";
+import { useState } from 'react';
+import styles from './style.module.scss';
+import Logo from '../../assets/Logo.svg';
+import { MdSearch, MdShoppingCart } from 'react-icons/md';
+import { CartModal } from '../CartModal';
 
-export const Header = () => {
-   const [value, setValue] = useState("");
+export const Header = ({ onCartButtonClick }) => {
+	const [value, setValue] = useState('');
 
-   return (
-      <header>
-         <img src={Logo} alt="Logo Kenzie Burguer" />
-         <div>
-            <button>
-                <MdShoppingCart size={21} />
-                <span>0</span>
-            </button>
-            <form>
-               <input
-                  type="text"
-                  value={value}
-                  onChange={(e) => setValue(e.target.value)}
-               />
-               <button type="submit">
-                 <MdSearch size={21} />
-               </button>
-            </form>
-         </div>
-      </header>
-   );
+	return (
+		<header className={styles.header}>
+			<div className={styles.headerContainer}>
+				<div className={styles.imgContainer}>
+					<img src={Logo} alt='Logo Kenzie Burguer' />
+				</div>
+				<button 
+					className={styles.cartButton} 
+					onClick={onCartButtonClick}
+				>
+					<MdShoppingCart size={21} />
+					<span className={styles.cartQuantity}>0</span>
+				</button>
+			</div>
+			<div className={styles.searchContainer}>
+				<form className={styles.formContainer}>
+					<input
+						className={styles.searchInput}
+						type='text'
+						value={value}
+						onChange={(e) => setValue(e.target.value)}
+						placeholder='Digitar Pesquisa'
+					/>
+					<button className={styles.searchButton} type='submit'>
+						<MdSearch size={21} />
+					</button>
+				</form>
+			</div>
+		</header>
+	);
 };
