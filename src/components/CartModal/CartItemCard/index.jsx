86ec5684170978c60766cfb044728bc7e6162bf9
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-04 05:45:13 -0300

    refactor: Redefinições gerais em Cart

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -1,15 +1,16 @@
-import { MdDelete } from "react-icons/md";
+import styles from './style.module.scss';
+import { MdDelete } from 'react-icons/md';
 
 export const CartItemCard = ({ product }) => {
-   return (
-      <li>
-         <div>
-            <img src={product.img} alt={product.name} />
-            <h3>{product.name}</h3>
-         </div>
-         <button aria-label="delete" title="Remover item">
-            <MdDelete size={21} />
-         </button>
-      </li>
-   );
+	return (
+		<li className={styles.container}>
+			<div>
+				<img src={product.img} alt={product.name} />
+				<h3>{product.name}</h3>
+			</div>
+			<button aria-label='delete' title='Remover item'>
+				<MdDelete size={21} />
+			</button>
+		</li>
+	);
 };
--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -1,33 +1,44 @@
-import { MdClose } from "react-icons/md";
-import { CartItemCard } from "./CartItemCard";
+import styles from './style.module.scss';
+import { MdClose } from 'react-icons/md';
+import { CartItemCard } from './CartItemCard';
 
-export const CartModal = ({ cartList }) => {
-   const total = cartList.reduce((prevValue, product) => {
-      return prevValue + product.price;
-   }, 0);
+export const CartModal = ({ cartList, onClose }) => {
+	const total = cartList.reduce((prevValue, product) => {
+		return prevValue + product.price;
+	}, 0);
 
-   return (
-      <div role="dialog">
-         <div>
-            <h2>Carrinho de compras</h2>
-            <button aria-label="close" title="Fechar">
-               <MdClose size={21} />
-            </button>
-         </div>
-         <div>
-            <ul>
-               {cartList.map((product) => (
-                  <CartItemCard key={product.id} product={product} />
-               ))}
-            </ul>
-         </div>
-         <div>
-            <div>
-               <span>Total</span>
-               <span>{total.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
-            </div>
-            <button>Remover todos</button>
-         </div>
-      </div>
-   );
+	return (
+		<div role='dialog' className={styles.container}>
+			<div className={styles.header}>
+				<h2 className={styles.title}>Carrinho de compras</h2>
+				<button 
+					className={styles.closeButton}
+					aria-label='close' 
+					title='Fechar'
+					onClick={onClose}
+				>
+					<MdClose size={21} />
+				</button>
+			</div>
+			<div className={styles.main}>
+				<ul className={styles.productList}>
+					{cartList.map((product) => (
+						<CartItemCard key={product.id} product={product} />
+					))}
+				</ul>
+			</div>
+			<div className={styles.footer}>
+				<div className={styles.totalContainer}>
+					<span className={styles.totalTitle}>Total</span>
+					<span className={styles.totalQuantity}>
+						{total.toLocaleString('pt-BR', {
+							style: 'currency',
+							currency: 'BRL',
+						})}
+					</span>
+				</div>
+				<button className={styles.removeButton}>Remover todos</button>
+			</div>
+		</div>
+	);
 };
