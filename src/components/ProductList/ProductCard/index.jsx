d8398c636b5650455f2c3c3c70e56c7386dc274d
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-05 05:26:06 -0300

    fix(product-list-style): Imgs de cards específicos reposicionados

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -4,13 +4,18 @@ export const ProductCard = ({ product }) => {
 	return (
 		<li className={styles.container}>
 			<div className={styles.imgContainer}>
-				<img src={product.img} alt={product.name} />
+				<img 
+					className={styles.img} 
+					src={product.img} 
+					alt={product.name} 
+					data-name={product.name}
+				/>
 			</div>
 			<div className={styles.description}>
 				<h3 className={styles.name}>{product.name}</h3>
 				<span className={styles.category}>{product.category}</span>
 				<span className={styles.price}>
-					{product.price.toLocaleString('pt-BR', {
+					{ product.price.toLocaleString('pt-BR', {
 						style: 'currency',
 						currency: 'BRL',
 					})}
